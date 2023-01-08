e42586edf42b9392e50f7b1f2bd958b120546eef
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-01-08 12:27:35 -0200

    Insert new Package

---
+++ TesteIntegracao2/src/Anexo2/ApenasParaAcompanharAula.java
@@ -0,0 +1,10 @@
+package Anexo2;
+
+public class ApenasParaAcompanharAula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Apenas para acompanhar a aula");
+	}
+
+}
