955042576ca9082356d5c36b4005b23e68addb80
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-01-08 11:02:26 -0300

    Add files via upload

---
+++ TesteIntegracao2/src/com/br/fabiorogerio/java/TesteDois.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.br.fabiorogerio.java;
+
+/**
+ * @author usuario
+ *
+ */
+public class TesteDois {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Segundo teste de integração de funcionalidades");
+		
+
+	}
+
+}
