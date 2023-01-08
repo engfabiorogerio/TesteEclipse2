34471cec75e130759acb815e6bdfde008883efd1
Fábio Rogério de Moraes e Silva <102857097+engfabiorogerio@users.noreply.github.com>
2023-01-08 12:09:06 -0200

    Insert name

--- TesteIntegracao2/src/com/br/fabiorogerio/java/TesteDois.java
+++ TesteIntegracao2/src/com/br/fabiorogerio/java/TesteDois.java
@@ -14,7 +14,7 @@ public class TesteDois {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Segundo teste de integração de funcionalidades");
-		
+		System.out.println("Fábio Rogério");
 
 	}
 
